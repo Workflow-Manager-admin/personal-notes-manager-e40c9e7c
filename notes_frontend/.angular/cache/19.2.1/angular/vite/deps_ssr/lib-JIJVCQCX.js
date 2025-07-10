c784b666a168ef586544e8cdb9b4c9fe0544badb
Kavia AI <production@kavia.ai>
2025-07-10 13:42:34 +0000

    Auto-commit from task cgbb23d3f6

--- notes_frontend/.angular/cache/19.2.1/angular/vite/deps_ssr/lib-JIJVCQCX.js
+++
@@ -1,6 +0,0 @@
-import { createRequire } from 'module';const require = createRequire(import.meta.url);
-import {
-  require_lib
-} from "./chunk-3JSCXER4.js";
-import "./chunk-ANGF2IQY.js";
-export default require_lib();
